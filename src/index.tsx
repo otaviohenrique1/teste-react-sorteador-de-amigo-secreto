a53a93146d88dadfd85d1ccc704c7b35dd28c97d
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2022-10-20 23:04:20 -0300

    Commit 3

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Cabecalho from './components/Cabecalho';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Cabecalho />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
