2b686a74d827374ae28ec6aaafa47ff9fc2250c8
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2022-10-20 22:54:56 -0300

    Commit 2

---
+++ src/components/Formulario.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Formulario = () => {
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="Insira os nomes dos participantes"
+      />
+      <button
+        type="button"
+        disabled={true}
+      >Adicionar</button>
+    </form>
+  );
+}
+
+export default Formulario;
