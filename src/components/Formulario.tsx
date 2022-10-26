ceeb5838d9ef35d607c467c055cea45b861df2c7
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2022-10-25 22:14:03 -0300

    Commit 13

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -1,6 +1,7 @@
 import React, { FormEvent, useRef, useState } from 'react';
 import { useAdicionarParticipante } from '../hook/useAdicionarParticipante';
 import { useMensagemDeErro } from '../hook/useMensagemDeErro';
+import './Formulario.css';
 
 const Formulario = () => {
   const [nome, setNome] = useState('');
@@ -20,18 +21,23 @@ const Formulario = () => {
 
   return (
     <form onSubmit={adicionarParticipante}>
-      <input
-        ref={inputRef}
-        type="text"
-        placeholder="Insira os nomes dos participantes"
-        value={nome}
-        onChange={event => setNome(event.target.value)}
-      />
-      <button
-        // type="button"
-        disabled={!nome}
-      >Adicionar</button>
-      {mensagemDeErro && <p role="alert">{mensagemDeErro}</p>}
+      <div  className="grupo-input-btn">
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder="Insira os nomes dos participantes"
+          value={nome}
+          onChange={event => setNome(event.target.value)}
+        />
+        <button
+          // type="button"
+          disabled={!nome}
+        >Adicionar</button>
+      </div>
+      {mensagemDeErro && <p
+        role="alert"
+        className="alerta erro"
+      >{mensagemDeErro}</p>}
     </form>
   );
 }
