e2dbfd2c9fd9d75137d14e361ba907f28401069d
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2022-10-26 16:02:47 -0300

    Commit 17

---
+++ src/helpers/realizarSorteio.test.ts
@@ -0,0 +1,12 @@
+import { realizarSorteio } from "./realizarSorteio";
+
+describe("dado um sorteio de amigo secreto", () => {
+  test('cada participante não sorteie o próprio nome', () => {
+    const participantes = ['Ana', 'Catarina', 'Juliana', 'João', 'Vinicius', 'Natalia'];
+    const sorteio = realizarSorteio(participantes);
+    participantes.forEach((participante) => {
+      const amigoSecreto = sorteio.get(participante);
+      expect(amigoSecreto).not.toEqual(participante);
+    });
+  })
+})
--- src/hook/useSorteador.ts
+++ src/hook/useSorteador.ts
@@ -1,20 +1,16 @@
-import shuffle from "just-shuffle";
+import { useSetRecoilState } from "recoil";
+import { realizarSorteio } from "../helpers/realizarSorteio";
+import { resultadoAmigoSecreto } from "../state/atom";
 import { useListaDeParticipantes } from "./useListaDeParticipantes";
 
 export const useSorteador = () => {
   const participantes = useListaDeParticipantes();
 
+  const setResultado = useSetRecoilState(resultadoAmigoSecreto);
+
   return () => {
-    const totalDeParticipantes = participantes.length;
+    const resultado = realizarSorteio(participantes);
 
-    const embaralhado = shuffle(participantes);
-
-    const resultado = new Map<string, string>();
-
-    for (let index = 0; index < totalDeParticipantes; index++) {
-      const indiceDoAmigo = index === (totalDeParticipantes - 1) ? 0 : index + 1;
-
-      resultado.set(embaralhado[index], embaralhado[indiceDoAmigo]);
-    }
+    setResultado(resultado);
   };
 };
